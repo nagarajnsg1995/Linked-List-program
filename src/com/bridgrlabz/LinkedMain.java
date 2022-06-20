365882e539855679387db755aa0eb7323bb2f10b
nagarajnsg1995 <nagarajsbg023@gmail.com>
2022-06-20 20:02:07 +0530

    Added:created simple linkedlist of 56,30,70

---
+++ src/com/bridgrlabz/LinkedMain.java
@@ -0,0 +1,13 @@
+package com.bridgrlabz;
+
+import java.util.LinkedList;
+
+public class LinkedMain {
+        public static void main(String[] args) {
+            LinkedList<Integer> number=new LinkedList<>();
+            number.add(56);
+            number.add(30);
+            number.add(70);
+            System.out.println( number);
+        }
+    }
